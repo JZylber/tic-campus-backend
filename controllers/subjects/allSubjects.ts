a8572c1844ed71185bba893342d0ad27e5ab90f2
JZylber <zylberjulian@gmail.com>
2026-02-20 17:31:12 -0300

    feat(db): students to prisma

--- controllers/subjects/allSubjects.ts
+++ controllers/subjects/allSubjects.ts
@@ -1,15 +1,5 @@
 import type { Request, Response } from "express";
-import { getSheetClient } from "../../connectors/google.ts";
-import { asTableData } from "../shared.ts";
 import { prisma } from "../../index.ts";
-
-type SubjectTemplateTable = Array<{
-  Materia: string;
-  Curso: string;
-  Año: string;
-  "Spreadsheet ID": string;
-  "Id Template": string;
-}>;
 
 type Subject = {
   name: string;
