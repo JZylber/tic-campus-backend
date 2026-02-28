47bb51dab950b01c6397b6ed88ca92f66e2021f2
JZylber <zylberjulian@gmail.com>
2026-02-27 22:23:22 -0300

    chore: changed cache policy

--- controllers/students/auth.ts
+++ controllers/students/auth.ts
@@ -3,26 +3,25 @@ import type { Request, Response } from "express";
 import { getSheetClient } from "../../connectors/google.ts";
 import Fuse from "fuse.js";
 import { getColumnIndex, setCacheHeaders } from "../shared.ts";
+import prisma from "../../prisma/prisma.ts";
 
 export async function getStudentData(
   request: Request<{}, {}, { name: string; surname: string; year: number }>,
-  response: Response
+  response: Response,
 ) {
-  const sheetsAPI = await getSheetClient();
-  const APIrequest = await sheetsAPI.spreadsheets.values.batchGet({
-    spreadsheetId: process.env.MAIN_SPREADSHEET_ID!,
-    ranges: ["EstudianteXCurso!A:E", "Estudiantes!A:E"],
-  });
-  const [studentCoursesRes, studentsRes] = APIrequest.data.valueRanges!;
   const { name, surname, year } = request.body;
-  const rowHeaders: string[] = studentsRes!.values?.shift() || [];
-  const students = studentsRes!.values?.map((row) => ({
-    id: row[getColumnIndex("Id", rowHeaders)],
-    dni: row[getColumnIndex("DNI", rowHeaders)],
-    mail: row[getColumnIndex("Mail", rowHeaders)],
-    surname: row[getColumnIndex("Apellido", rowHeaders)],
-    name: row[getColumnIndex("Nombre", rowHeaders)],
-  }));
+  const students = await prisma.user.findMany({
+    where: {
+      studentCourses: {
+        some: {
+          year: year,
+        },
+      },
+    },
+    include: {
+      studentCourses: true,
+    },
+  });
   const fuse = new Fuse(students!, {
     keys: ["name", "surname"],
     threshold: 0.4,
@@ -34,14 +33,14 @@ export async function getStudentData(
       .send({ message: "No student found with the given name and surname." });
   }
   let studentId = possibleStudents[0]!.item.id;
-  const currentCourse = studentCoursesRes!.values?.find(
-    (row) => row[0] === studentId && Number(row[3]) === year
-  )?.[4];
+  let currentCourse = possibleStudents[0]!.item.studentCourses.find(
+    (course) => course.year === year,
+  )?.course;
   // IF year is 2025, use DNI as student ID
   if (year === 2025) {
-    studentId = possibleStudents[0]!.item.dni.toString();
+    studentId = parseInt(possibleStudents[0]!.item.dni);
   }
-  // Set cache headers for 1 hour
-  setCacheHeaders(response, 3600);
+  // Set cache headers for 10 minutes
+  setCacheHeaders(response, 600);
   return response.status(200).send({ id: studentId, course: currentCourse });
 }
